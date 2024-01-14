542c237059baa420e44ff8539a09c740968fe946
David Choi <dharric@live.com>
2024-01-14 11:38:09 -0500

    chore: add gitignore to api and start api lib

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -18,3 +18,20 @@ pub mod routes {
     pub mod route_utils;
     pub mod stripped_down_error;
 }
+
+use crate::routes::app_state::AppState;
+use std::env;
+use dotenv::dotenv;
+use openssl::ssl::{SslAcceptorBuilder, SslAcceptor, SslMethod, SslFiletype};
+
+#[allow(unused)]
+fn ssl_builder() -> SslAcceptorBuilder {
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("ssl/key.pem", SslFiletype::PEM)
+        .expect("failed to open/read key.pem");
+    builder.set_certificate_chain_file("ssl/cert.pem")
+        .expect("failed to open/read cert.pem");
+    builder
+}
+
