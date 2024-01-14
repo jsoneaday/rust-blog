ef49a78b830cad7fc072acf64eb0cc9d81bc8d1f
David Choi <dharric@live.com>
2024-01-14 11:47:06 -0500

    chore: setup lib file

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -21,6 +21,9 @@ pub mod routes {
 
 use crate::routes::app_state::AppState;
 use std::env;
+use actix_cors::Cors;
+use actix_web::{HttpServer, App, http::header, middleware::Logger};
+use common::{repository::base::{DbRepo, Repository}, authentication::auth_service::{AuthService, init_auth_keys}};
 use dotenv::dotenv;
 use openssl::ssl::{SslAcceptorBuilder, SslAcceptor, SslMethod, SslFiletype};
 
@@ -35,3 +38,37 @@ fn ssl_builder() -> SslAcceptorBuilder {
     builder
 }
 
+pub async fn run() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("debug"));
+    
+    dotenv().ok();
+    let host = env::var("HOST").unwrap();
+    let port = env::var("PORT").unwrap().parse::<u16>().unwrap();
+    
+    let app_data = actix_web::web::Data::new(AppState{
+        repo: DbRepo::init().await,
+        auth_service: AuthService,
+        auth_keys: init_auth_keys().await
+    });    
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(app_data.clone())     
+            .wrap(Logger::default())
+            .wrap(
+                Cors::default()
+                    .allowed_origin("http://localhost:8080")
+                    .allowed_methods(vec!["GET", "POST"])
+                    .allowed_headers(vec![
+                        header::CONTENT_TYPE,
+                        header::AUTHORIZATION,
+                        header::ACCEPT, // todo: might not need this
+                    ])
+                    .supports_credentials()
+                    .max_age(3600)
+            )          
+    })
+    .bind((host, port)).expect("")
+    .run()
+    .await
+}
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+use rust_blog_api::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    run().await
 }
