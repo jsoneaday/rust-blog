3eb6a6033b635199df7ffc177b1360c702095c09
David Choi <dharric@live.com>
2024-01-15 22:16:20 -0500

    chore: start administrator e2e test

--- api/src/common/repository/administrator/repo.rs
+++ api/src/common/repository/administrator/repo.rs
@@ -22,6 +22,20 @@ mod internal {
         }
     }
 
+    pub async fn insert_administrator(
+        conn: &Pool<Postgres>,
+        user_name: String,
+        email: String,
+        password: String
+    ) -> Result<EntityId, Error> {
+        query_as::<_, EntityId>("insert into administrator (user_name, email, password) values ($1, $2, $3) returning id")
+            .bind(user_name)
+            .bind(email)
+            .bind(password)
+            .fetch_one(conn)
+            .await
+    }
+
     pub async fn query_administrator(conn: &Pool<Postgres>, id: i64) -> Result<Option<Administrator>, Error> {
         query_as::<_, Administrator>("select * from administrator where id = $1")
             .bind(id)
@@ -39,6 +53,18 @@ pub trait AuthenticateDbFn {
 impl AuthenticateDbFn for DbRepo {
     async fn authenticate_db(&self, email: String, password: String) -> Result<AuthenticateResult, sqlx::Error> {
         internal::authenticate_db(self.get_conn(), email, password).await
+    }
+}
+
+#[async_trait]
+pub trait InsertAdminisratorFn {
+    async fn insert_administrator(&self, user_name: String, email: String, password: String) -> Result<EntityId, Error>;
+}
+
+#[async_trait]
+impl InsertAdminisratorFn for DbRepo {
+    async fn insert_administrator(&self, user_name: String, email: String, password: String) -> Result<EntityId, Error> {
+        internal::insert_administrator(self.get_conn(), user_name, email, password).await
     }
 }
 
--- api/tests/common/authentication/test_auth_service.rs
+++ api/tests/common/authentication/test_auth_service.rs
@@ -11,7 +11,7 @@ async fn test_is_authenticated() {
     let user_name = "jon".to_string();
     
     let req = get_fake_httprequest_with_bearer_token(
-        user_name.clone(), &app_data.auth_keys.encoding_key, "/v1/developer", 1, Some(60*2), None
+        user_name.clone(), &app_data.auth_keys.encoding_key, "/v1/administrator", 1, Some(60*2), None
     );
     let headers = get_header_strings(req.headers());
 
