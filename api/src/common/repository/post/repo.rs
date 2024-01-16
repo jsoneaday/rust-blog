ba7faa31629640333c9b4039545157fa3ca9d0d6
David Choi <dharric@live.com>
2024-01-16 14:05:25 -0500

    chore: add post tests

--- api/src/common/repository/post/repo.rs
+++ api/src/common/repository/post/repo.rs
@@ -1,9 +1,17 @@
 use async_trait::async_trait;
 use sqlx::{Postgres, Pool, query_as, Error};
-use crate::common::repository::{post::models::Post, base::{DbRepo, ConnGetter}};
+use crate::common::repository::{post::models::Post, base::{DbRepo, ConnGetter, EntityId}};
 
 mod internal {
     use super::*;
+
+    pub async fn insert_post(conn: &Pool<Postgres>, message: String, admin_id: i64) -> Result<EntityId, Error> {
+        query_as::<_, EntityId>("insert into post (message, admin_id) values ($1, $2) returning id")
+            .bind(message)
+            .bind(admin_id)
+            .fetch_one(conn)
+            .await
+    }
 
     pub async fn query_posts(conn: &Pool<Postgres>, page_size: i32, last_offset: i64) -> Result<Vec<Post>, Error> {
         query_as::<_, Post>("select * from post order by updated_at desc limit $1 offset $2")
@@ -11,6 +19,18 @@ mod internal {
             .bind(last_offset)
             .fetch_all(conn)
             .await
+    }
+}
+
+#[async_trait]
+pub trait InsertPostFn {
+    async fn insert_post(&self, message: String, admin_id: i64) -> Result<EntityId, Error>;
+}
+
+#[async_trait]
+impl InsertPostFn for DbRepo {
+    async fn insert_post(&self, message: String, admin_id: i64) -> Result<EntityId, Error> {
+        internal::insert_post(self.get_conn(), message, admin_id).await
     }
 }
 
---
+++ api/tests/common/repository/post/test_post.rs
@@ -0,0 +1,32 @@
+use rust_blog_api::{
+    common::{repository::{base::{DbRepo, Repository}, administrator::repo::InsertAdminisratorFn, post::repo::{InsertPostFn, QueryPostsFn}}, authentication::auth_service::AuthService}, 
+    common_test::fixtures::get_app_data
+};
+use fake::{Fake, faker::{internet::en::{Username, SafeEmail, Password}, lorem::en::Sentence}};
+
+#[tokio::test]
+async fn test_query_posts_return_correct_posts() {
+    let repo = DbRepo::init().await;
+    let app_data = get_app_data(repo, AuthService).await;
+    let message = Sentence(1..2).fake::<String>();
+
+    let entity_admin_result = app_data.repo.insert_administrator(Username().fake::<String>(), SafeEmail().fake::<String>(), Password(5..10).fake::<String>()).await.unwrap();
+    _ = app_data.repo.insert_post(message.clone(), entity_admin_result.id).await.unwrap();
+    let post_result = app_data.repo.query_posts(10, 0).await.unwrap();
+    
+    assert!(post_result.len() > 0);
+    assert!(post_result.first().unwrap().message == message);
+    assert!(post_result.first().unwrap().admin_id == entity_admin_result.id);
+}
+
+#[tokio::test]
+async fn test_insert_post_returns_valid_admin() {
+    let repo = DbRepo::init().await;
+    let app_data = get_app_data(repo, AuthService).await;
+    let message = Sentence(1..2).fake::<String>();
+
+    let entity_admin_result = app_data.repo.insert_administrator(Username().fake::<String>(), SafeEmail().fake::<String>(), Password(5..10).fake::<String>()).await.unwrap();
+    let entity_post_result = app_data.repo.insert_post(message.clone(), entity_admin_result.id).await.unwrap();
+    
+    assert!(entity_post_result.id > 0);
+}
