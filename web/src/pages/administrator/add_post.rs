a5e4b1138ab55728f72468872a8879eb12765cb7
David Choi <dharric@live.com>
2024-01-24 16:56:26 -0500

    chore: add add post screen; anchor highlight issues;

--- web/src/pages/administrator/add_post.rs
+++ web/src/pages/administrator/add_post.rs
@@ -1,10 +1,37 @@
 use leptos::*;
+use leptos::logging::log;
+use leptos_router::*;
 
 #[component]
 pub fn AddPost() -> impl IntoView {
+    let (title, set_title) = create_signal("".to_string());
+    let (content, set_content) = create_signal("".to_string());
+
     view! {
         <div class="home-content">
-            "add post"
+            <h2>"Add Post"</h2>
+            <Form method="post" action="/dosomething">
+                <section class="form-section">
+                    <label for="title">
+                        "Title"                    
+                    </label>
+                    <input 
+                        type="text" 
+                        id="title"
+                        on:input=move |ev| {
+                            log!("target value: {}", event_target_value(&ev));
+                            set_title(event_target_value(&ev));
+                        } 
+                        prop:value=title
+                    />
+                </section>
+                <section class="form-section" style="height: 600px">
+                    <label for="content">
+                        "Content"                    
+                    </label>
+                    <textarea />
+                </section>
+            </Form>
         </div>
     }
 }
--- web/src/pages/administrator/admin.rs
+++ web/src/pages/administrator/admin.rs
@@ -1,10 +1,17 @@
 use leptos::*;
+use leptos::logging::log;
 use leptos_router::*;
+use web_sys::console::log;
 use crate::common::components::layout::Layout;
+
+const MAIL: &str = "/admin/mail";
+const ADD_POST: &str = "/admin/addpost";
+const MNG_POST: &str = "/admin/mngpost";
 
 #[component]
 pub fn Admin() -> impl IntoView {
     let location = use_location();
+    log!("pathname: {}", location.pathname.get());
     let (current_selected_nav, set_current_selected_nav) = create_signal(location.pathname.get());
     
     view! {
@@ -13,13 +20,19 @@ pub fn Admin() -> impl IntoView {
                 <h2>Administration</h2>
                 <ul>
                     <li>
-                        <a href="/admin/mail" class=("a-selected", move || current_selected_nav() == "mail" )>"Mail"</a>
+                        <a href={MAIL} class=("a-selected", move || current_selected_nav() == MAIL ) on:click=move |_| {
+                            set_current_selected_nav(MAIL.to_string());
+                        }>"Mail"</a>
                     </li>
                     <li>
-                        <a href="/admin/addpost" class=("a-selected", move || current_selected_nav() == "addpost" )>"Add Post"</a>
+                        <a href={ADD_POST} class=("a-selected", move || current_selected_nav() == ADD_POST ) on:click=move |_| {
+                            set_current_selected_nav(ADD_POST.to_string());
+                        }>"Add Post"</a>
                     </li>
                     <li>
-                        <a href="/admin/mngpost" class=("a-selected", move || current_selected_nav() == "mngpost" )>"Manage Posts"</a>
+                        <a href={MNG_POST} class=("a-selected", move || current_selected_nav() == MNG_POST ) on:click=move |_| {
+                            set_current_selected_nav(MNG_POST.to_string());
+                        }>"Manage Posts"</a>
                     </li>
                 </ul>
             </nav>
