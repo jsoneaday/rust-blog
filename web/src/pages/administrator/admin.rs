cce0ca7df83c6107fedb6c84379e29983e2d92ae
David Choi <dharric@live.com>
2024-01-24 13:54:54 -0500

    fix: admin current_selected_nav

--- web/src/pages/administrator/admin.rs
+++ web/src/pages/administrator/admin.rs
@@ -5,7 +5,7 @@ use crate::common::components::layout::Layout;
 #[component]
 pub fn Admin() -> impl IntoView {
     let location = use_location();
-    let (current_selected_nav, set_current_selected_nav) = create_signal(location.pathname);
+    let (current_selected_nav, set_current_selected_nav) = create_signal(location.pathname.get());
     
     view! {
         <Layout>            
@@ -13,13 +13,13 @@ pub fn Admin() -> impl IntoView {
                 <h2>Administration</h2>
                 <ul>
                     <li>
-                        <a href="/admin/mail" class=("a-selected", move || current_selected_nav().get() == "mail" )>"Mail"</a>
+                        <a href="/admin/mail" class=("a-selected", move || current_selected_nav() == "mail" )>"Mail"</a>
                     </li>
                     <li>
-                        <a href="/admin/addpost" class=("a-selected", move || current_selected_nav().get() == "addpost" )>"Add Post"</a>
+                        <a href="/admin/addpost" class=("a-selected", move || current_selected_nav() == "addpost" )>"Add Post"</a>
                     </li>
                     <li>
-                        <a href="/admin/mngpost" class=("a-selected", move || current_selected_nav().get() == "mngpost" )>"Manage Posts"</a>
+                        <a href="/admin/mngpost" class=("a-selected", move || current_selected_nav() == "mngpost" )>"Manage Posts"</a>
                     </li>
                 </ul>
             </nav>
